dfb064d5ecffaa56a07779cd02d2ff2095d8ae17
Miracle <002nasya@gmail.com>
2024-09-15 11:51:32 +0200

    test program

--- extractors.py
+++ extractors.py
@@ -1,11 +1,10 @@
 import re
 
 def extract_emails(text):
-        # Define the regular expression for matching email addresses
-            email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
-                
-                    # Use re.findall() to find all email addresses in the text
-                        emails = re.findall(email_pattern, text)
-                            
-                                return emails
-
+    # Define the regular expression for matching email addresses
+    email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+    
+    # Use re.findall() to find all email addresses in the text
+    emails = re.findall(email_pattern, text)
+    
+    return emails

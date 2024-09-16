80b2c9b5eb0836be0478c434e9f647fad1225c49
Nformi Modestine Girbong <n.girbong@alustudent.com>
2024-09-16 16:19:48 +0200

    first

--- Hashtags.py
+++ Hashtags.py
@@ -16,7 +16,7 @@ def main():
         for match in matches:
             print(match)
     else:
-        print("Sorry No hashtags found.")
+        print("Sorry! No hashtags found.")
         
 
     
